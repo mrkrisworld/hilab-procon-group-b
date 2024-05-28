cffa21f22930824dd0f3d89c34a4cb75bc9a52dd
mrkrisworld <louis6198@yahoo.com>
2024-05-28 16:30:22 +0900

    add @nuxtjs/dayjs dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/day.js',
-    '~plugins/full_calendar.js'
+    '~/plugins/day.js',
+    '~/plugins/full_calendar.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
