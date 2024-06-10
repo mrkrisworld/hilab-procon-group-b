45abd5efbc9b7d32d7edec6242b7a6aef3ef67cb
mrkrisworld <louis6198@yahoo.com>
2024-06-10 15:56:50 +0900

    Add CSS Stylings
    Add color definitions and custom fonts.

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/css/styles.scss',
+    '~/assets/css/fonts.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -50,7 +52,9 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: [
+      '~/assets/css/variables.scss'
+    ],
     theme: {
       dark: true,
       themes: {
