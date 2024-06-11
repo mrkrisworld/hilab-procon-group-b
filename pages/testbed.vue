7ac8be64ee20f9d22d7ad138a20d4daf6140ec9e
mrkrisworld <louis6198@yahoo.com>
2024-06-11 15:45:56 +0900

    Add hidden testbed, change page lang to Japanese

---
+++ pages/testbed.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <TheLogo :headerText=headerText />
+      <!--ENTER TEST COMPONENT HERE-->
+      <ComponentNewDate
+        style="border: 5px; border-color: gray;"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import ComponentNewDate from '../components/ComponentNewDate.vue';
+import TheLogo from '../components/TheLogo.vue';
+  
+  export default {
+    data () {
+      return {
+        headerText: 'Testbed'
+      }
+    },
+    head () {
+      return {
+        title: 'dev'
+      }
+    }
+  }
+  </script>
+  
+  <style lang="scss">
+
+  </style>
+  
